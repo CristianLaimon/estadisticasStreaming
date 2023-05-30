e49723199085f4d253eda23d7185c50ecb35e3ce
Kristan Ruíz <itan_07@hotmail.com>
2023-05-30 15:46:19 -0800

    Error chistoso en forma de escalera al leer el archivo.
    Se debe a que se está agregando fila por cada dato cuando debería ser por cada fila de datos. jaja, que chistso

--- Form1.cs
+++ Form1.cs
@@ -15,13 +15,20 @@ namespace estadisticasStreaming
 
         private void button1_Click(object sender, EventArgs e) => ObtenerRuta();
         private void abrirNuevoArchivoToolStripMenuItem_Click(object sender, EventArgs e) => ObtenerRuta();
-        private void abrirConsumotxtToolStripMenuItem_Click(object sender, EventArgs e) => rutaArchivo = Path.Combine(Application.StartupPath, "Data", "consumo.txt");
+
+        private void abrirConsumotxtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rutaArchivo = Path.Combine(Application.StartupPath, "Data", "consumo.txt");
+            LeerArchivo();
+        }
+        
 
         private void ObtenerRuta()
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 rutaArchivo = openFileDialog1.FileName;
+                LeerArchivo();
             }
         }
 
@@ -29,21 +36,35 @@ namespace estadisticasStreaming
         {
             if (rutaArchivo != null)
             {
+                int fila = 0;
+
                 using (FileStream fs = new FileStream(rutaArchivo, FileMode.Open, FileAccess.ReadWrite))
                 {
-                    var br = new BinaryReader(fs);
-                    var bw = new BinaryWriter(fs);
+                    var escritor = new StreamWriter(fs);
+                    var lector = new StreamReader(fs);
 
-                    string rawText = 
+                    while (!lector.EndOfStream)
+                    {
+                        string linea = lector.ReadLine();
+                        string[] lineaElementos = linea.Split(',');
 
+                        for (int i = 0; i < lineaElementos.Length - 1; i++) //Es -1 para que se consideren los índices de las filas, ya que estos empiezan en 0 y no en 1.
+                        {
+                            dataGridView1.Rows.Add();
+                            dataGridView1.Rows[fila].Cells[i].Value = lineaElementos[i];
+                        fila++;
+                        }
+                    }
                 }
 
             }
             else
             {
-                MessageBox.Show("Todavía no ha seleccionado un archivo para leer", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                MessageBox.Show("Todavía no ha seleccionado un archivo para leer", "Advertencia", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
             }
         }
+
 
 
     }
