a5f3850a098a89783f25ca259b717734c7efc020
DianaSesma <yulissasesma730@gmail.com>
2023-06-04 14:55:35 -0700

    agregando mensaje al seleccionar tipo de grafica

--- Formsitos/VentanasAjustes/AOtros.cs
+++ Formsitos/VentanasAjustes/AOtros.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,18 +21,34 @@ namespace estadisticasStreaming.Formsitos.VentanasAjustes
             InitializeComponent();
         }
 
-        private void buttonAplicar_Click(object sender, EventArgs e)
-        {
-            if (comboBox1.SelectedItem != null && comboBox1.SelectedItem.ToString() == "Pastel") Estadisticas.TipoGrafica = "Pastel";
-            else if (comboBox1.SelectedItem != null && comboBox1.SelectedItem.ToString() == "Barras") Estadisticas.TipoGrafica = "Barras";
-            
-            else if (comboBox1.SelectedItem == null) MessageBox.Show("No ha seleccionado nada", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            
-        }
-
         private void AOtros_Load(object sender, EventArgs e)
         {
+            label1.Text = "";
             comboBox1.Text = "Barras";
+        }
+
+        private void buttonAplicar_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null && comboBox1.SelectedItem.ToString() == "Pastel")
+            {
+                label1.Text = "Aplicado";
+                timer1.Start();
+                Estadisticas.TipoGrafica = "Pastel";
+            }
+            else if (comboBox1.SelectedItem != null && comboBox1.SelectedItem.ToString() == "Barras")
+            {
+                label1.Text = "Aplicado";
+                timer1.Start();
+                Estadisticas.TipoGrafica = "Barras";
+            }
+
+            else if (comboBox1.SelectedItem == null) MessageBox.Show("No ha seleccionado nada", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            timer1.Stop();
         }
     }
 }
