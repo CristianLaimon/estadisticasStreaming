f6e878e73db67db71956d254992a0b515e8fe048
DianaSesma <yulissasesma730@gmail.com>
2023-06-04 13:13:45 -0700

    agregando label de aplicado (cuando selecciona el tipo de grafica)

--- Formsitos/VentanasAjustes/AOtros.Designer.cs
+++ Formsitos/VentanasAjustes/AOtros.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.buttonAplicar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // comboBox1
@@ -53,11 +56,26 @@
             this.buttonAplicar.UseVisualStyleBackColor = true;
             this.buttonAplicar.Click += new System.EventHandler(this.buttonAplicar_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // AOtros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(332, 134);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonAplicar);
             this.Controls.Add(this.comboBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -65,6 +83,7 @@
             this.Text = "Tipo de Grafica";
             this.Load += new System.EventHandler(this.AOtros_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +91,7 @@
 
         private ComboBox comboBox1;
         private Button buttonAplicar;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
